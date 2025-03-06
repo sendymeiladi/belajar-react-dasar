497fcbdf7feb0cce6c6478ec8ffdf9c9ef2d636a
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-06 13:50:37 +0700

    feat : sharing State

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 export default function Task() {
     const [items, setItems] = useImmer([]);
 
-    function handleClick(e){
-        e.preventDefault();
-        setItems(draft => {
+    function handleOnSubmit(item){
+        setItems((draft) => {
             draft.push(item);
         });
-        setItem("");
     }
 
     return (
         <div>
-            <h1>Create Task</h1>
-            <form>
-                <input value={item} onChange={handelChange} />
-                <button onClick={handleClick}>Add</button>
-            </form>
-            <h1>List Task</h1>
-            <ul>
-                {items.map((item, index) =>
-                    <li key={index}>{item}</li>
-                )}
-            </ul>
+           <TaskForm onSubmit={handleOnSubmit} />
+           <TaskList items={items} />
         </div>
     )
 }
