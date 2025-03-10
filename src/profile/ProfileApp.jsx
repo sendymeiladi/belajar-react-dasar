b40311338ff754ec7bdfbd2f2b5ea7e1adca6061
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-10 11:51:21 +0700

    learn context and state

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Profile from "./Profile";
 import ProfileAddress from "./ProfileAddress";
 import { ProfileContext } from "./ProfileContext";
+import ProfileForm from "./ProfileForm";
 
 export default function ProfileApp(){
+    const [name, setName] = useState("Eko");
+
     return(
         <>
-            <ProfileContext.Provider value="Sendy">
+            <ProfileContext.Provider value={name}>
                 <h1>Profile App</h1>
+                <ProfileForm name={name} setName={setName}/>
                 <Profile/>
                 <ProfileAddress/>
             </ProfileContext.Provider>
